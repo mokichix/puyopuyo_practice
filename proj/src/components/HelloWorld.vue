04d325e34c4b2c1dc7c0d716d7376a6ac6be0464
Mokichi_x <mokichikichikichi@dummy.com>
2020-11-06 23:31:01 +0900

    Add layout

--- proj/src/components/HelloWorld.vue
+++ proj/src/components/HelloWorld.vue
@@ -1,33 +1,17 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div class="main-field">
+  <div class="left-field">
+    <div class="ojama-field">
+    </div>
+    <div class="play-field">
+      <table class="play-field-table">
+        <tr><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+      </table>
+    </div>
   </div>
+  <div class="next-field">
+  </div>
+</div>
 </template>
 
 <script>
@@ -55,4 +39,31 @@ li {
 a {
   color: #42b983;
 }
+.main-field {
+  height: 100%;
+  display: flex;
+}
+.left-field {
+  width: 50%;
+}
+.ojama-field {
+  height: 20%;
+  background-color: gray;
+}
+.play-field {
+  background-color: lightgrey;
+  height: 80%;
+}
+.play-field-table > tr > td {
+  border: solid 5px black;
+  border-color: black;
+  width: 18%;
+  height: 8.333%;
+}
+.next-field {
+  border: solid 1px black;
+  height: 25%;
+  width: 50%;
+}
+
 </style>
